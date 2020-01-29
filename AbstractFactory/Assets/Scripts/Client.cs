f46734f5949f90acaf6e12e42cc82970c2793061
Devin Hicks <devinhicks.az@gmail.com>
2020-01-29 11:59:28 -0700

    initial commit

---
+++ AbstractFactory/Assets/Scripts/Client.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Client : MonoBehaviour
+{
+    public int SkillLevel;
+    public int NumberOfGuests;
+    public bool IsVegan;
+
+    public Slider skillSlider;
+    public Slider guestSlider;
+    public Slider cheeseToggle;
+
+    public Canvas questionsUI;
+
+    private DinnerRequirements requirements;
+
+    void Start()
+    {
+        requirements = gameObject.AddComponent<DinnerRequirements>();
+
+        requirements.NumberOfGuests = Mathf.Max(NumberOfGuests);
+        requirements.IsVegan = IsVegan;
+        requirements.SkillLevel = Mathf.Max(SkillLevel);
+    }
+
+    public void MakeDinner()
+    {
+        requirements.NumberOfGuests = (int)guestSlider.value;
+        requirements.SkillLevel = (int)skillSlider.value;
+        if (cheeseToggle.value == 1)
+        {
+            requirements.IsVegan = false;
+        }
+        else
+        {
+            requirements.IsVegan = true;
+        }
+
+        questionsUI.gameObject.SetActive(false);
+
+        DinnerFactory factory = new DinnerFactory(requirements);
+        IDinner d = factory.Create();
+        GameObject.Instantiate(Resources.Load(d + "Prefab"));
+    }
+}
---
+++ AbstractFactory/Assets/Scripts/DinnerFactory.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDinnerFactory
+{
+    IDinner Create(DinnerRequirements requirements);
+}
+
+//public abstract class AbstractDinnerFactory
+//{
+//    public abstract ICheesey GetCheesey(DinnerRequirements requirements);
+//    public abstract IVegan GetVegan(DinnerRequirements requirements);
+//}
+
+public class DinnerFactory //: AbstractDinnerFactory
+{
+    private readonly IDinnerFactory _factory;
+    private readonly DinnerRequirements _requirements;
+
+    public DinnerFactory(DinnerRequirements requirements)
+    {
+        _factory = requirements.IsVegan ?
+            (IDinnerFactory)new Vegan() : new Cheesey();
+        _requirements = requirements;
+    }
+
+    public IDinner Create()
+    {
+        return _factory.Create(_requirements);
+    }
+}
+
+public class Cheesey : IDinnerFactory
+{
+    public IDinner Create(DinnerRequirements requirements)
+    {
+        switch (requirements.SkillLevel)
+        {
+            case 1:
+                if (requirements.NumberOfGuests >= 3) return new Pizza();
+                return new MacAndCheese();
+            case 2:
+                return new Cheeseburger();
+            case 3:
+                return new Souffle();
+            default:
+                return new MacAndCheese();
+        }
+    }
+}
+
+public class Vegan : IDinnerFactory
+{
+    public IDinner Create(DinnerRequirements requirements)
+    {
+        switch (requirements.SkillLevel)
+        {
+            case 1:
+                if (requirements.NumberOfGuests >= 3) return new Bruschetta();
+                return new Tofu();
+            case 2:
+                return new VeggieStew();
+            case 3:
+                return new Ratatouille();
+            default:
+                return new Tofu();
+        }
+    }
+}
